b4abeb984151055949590e1296536969716c3e73
Andrew McKinnon <andrewmckinnon2@gmail.com>
2017-09-06 15:01:04 -0400

    Added new structuring of database on javascript for writing a review

--- public/javascript/viewReview.js
+++ public/javascript/viewReview.js
@@ -4,43 +4,80 @@ $("#writeReview").click(function(){
   window.location="../html/writeReview.html";
 })
 
+//Class to be placed into arrays, holding average ratings for the various facets of a dorm
+class bathroomObj{
+  constructor(dormName, bathroom, cleanliness, gym, kitchen, party, room, study){
+    this.dormName = dormName;
+    this.bathroom = bathroom;
+    this.cleanliness = cleanliness;
+    this.gym = gym;
+    this.kitchen = kitchen;
+    this.party = party;
+    this.room = room;
+    this.study = study;
+  }
+
+  getDormName(){
+    return dormName;
+  }
+}
+
 var dormNames = ["Alderman", "Alexander", "Avery", "Aycock", "Carmichael",
-"Cobb","Connor", "Craige", "Craige-North", "Ehringhaus", "Everett", "Graham", "Grimes",
-"Hardin", "Hinton-James", "Horton", "Joyner", "Kenan", "Koury", "Lewis", "Mangum", "Manly",
-"McIver", "Morrison", "Old-East", "Old-West", "Parker", "Ruffin", "Spencer", "Stacy", "Teague",
-"Winston"];
+"Cobb","Connor", "Craige", "Craige North", "Ehringhaus", "Everett", "Graham", "Grimes",
+"Hardin", "Hinton James", "Horton", "Joyner", "Kenan", "Koury", "Lewis", "Mangum", "Manly",
+"McIver", "Morrison", "Old East", "Old West", "Parker", "Ruffin", "Spencer", "Stacy", "Teague",
+"Winston", "Granville"];
 
 for(var i=0; i<dormNames.length; i++){
-  $("#dormName").append("<option id=\"" + dormNames[i].toLowerCase() + "\">" + dormNames[i] + "</option>");
+  $("#dormsearch").append("<div class=\'ranking\'><div class=\'number\'><h12>" + i + "</h12></div>" +
+  "<div class=\'name\'><h11>" + dormNames[i] + "</h11></div>" + "<div class=\'overallscore\'><h13>" + 1.0 + "</h13></div></div>");
 }
 
 var currentDormSelected = "Alderman"
 $("#reviewHolder").empty();
 getCurrentInfo();
 
-$("#dormName").change(function(){
-  currentDormSelected = $("#dormName").find(":selected").text()
+$(".ranking").click(function(){
+  currentDormSelected = $(this).children(".name").children("h11").text()
+  console.log("dorm that was clicked is " + currentDormSelected)
+  $("#closeReviewNav").click();
   $("#reviewHolder").empty();
+  $(".dormtitle > h8").empty();
+  $(".dormtitle > h2").text(currentDormSelected);
   console.log(currentDormSelected);
   getCurrentInfo();
+})
+
+$("#sortButton").click(function(){
+
 })
 
 function getCurrentInfo(){
   var numReviews;
   var topTotalScorePossible;
+  var dormStyle;
+  var laundry;
+  var population;
   var grandScore = 0;
   var grandStudyDist = 0;
   var grandPartyDist = 0;
   var grandGymDist = 0;
+  var grandKitchenScore = 0;
 
-  firebase.database().ref("/ratings/" + currentDormSelected + "/").once("value").then(function(snap){
-    numReviews = snap.val().numReviews;
-    topTotalScorePossible = topPossibleScore * numReviews;
-    console.log(numReviews);
+  firebase.database().ref("UNC-CH/ratings/" + currentDormSelected + "/Objective Info/").once("value").then(function(snapshot){
+    population = snapshot.child("Population").val();
+    laundry = snapshot.child("Laundry").val();
+    dormStyle = snapshot.child("Dorm Style");
   }).then(function(){
-    firebase.database().ref("/ratings/" + currentDormSelected + "/").once("value").then(function(snap){
+    firebase.database().ref("UNC-CH/ratings/" + currentDormSelected + "/").once("value").then(function(snap){
+      numReviews = snap.val().numReviews;
+      topTotalScorePossible = topPossibleScore * numReviews;
+      console.log(numReviews);
+    })
+  }).then(function(){
+    firebase.database().ref("UNC-CH/ratings/" + currentDormSelected + "/").once("value").then(function(snap){
       snap.forEach(function(childSnap){
-        if(childSnap.key != "numReviews"){
+        if(childSnap.key != "numReviews" && childSnap.key !="Objective Info"){
           var ratingInfo = childSnap.val();
           console.log(ratingInfo);
           var bathroom = parseInt(ratingInfo.bathroom);
@@ -49,9 +86,17 @@ function getCurrentInfo(){
           var writtenReview = ratingInfo.review;
           var name = ratingInfo.name;
           var culture = ratingInfo.culture;
-          var partyDist = ratingInfo.partyProximity;
-          var studyDist = ratingInfo.studyProximity;
-          var gymDist = ratingInfo.gymProximity;
+          var date = ratingInfo.date;
+          var year = ratingInfo.year;
+          var room = ratingInfo.room;
+          var partyDist = parseInt(ratingInfo.partyProximity);
+          var studyDist = parseInt(ratingInfo.studyProximity);
+          var gymDist = parseInt(ratingInfo.gymProximity);
+          var overall = Math.round(((bathroom + cleanliness + kitchen + partyDist + studyDist + gymDist)/(15*6))*100);
+          console.log("overall for rating is " + overall);
+          var roomPerc = Math.round((room/15)*100);
+          var bathroomPerc = Math.round((bathroom/15)*100);
+          var kitchenPerc = Math.round((kitchen/15)*100);
 
           var clean = "dirty";
           if(cleanliness >=7){
@@ -65,30 +110,59 @@ function getCurrentInfo(){
           grandPartyDist = grandPartyDist + partyDist;
           grandStudyDist = grandStudyDist + studyDist;
           grandGymDist = grandGymDist + gymDist;
+          grandKitchenScore = grandKitchenScore + kitchen;
 
-
-          $("#reviewHolder").append("<div class=\"comment\"> <div class=\"tagscontainer\">" + "<div class=\"tagstitle\"><p5>" +
-            name + "</p5></div> <div class=\"tag\"><p5>" + culture + "</p5></div> <div class=\"tag\"><p5>" + clean +
-            "</p5></div> </div>" + "<div class=\"commentcontainer\"> <div class=\"dormRating\"> <div class=\"overall1\"><p3>" +
-            "Overall Score:" + "</p3></div> <div class=\"indiv1\"><p4>" + weightedScorePercent + "%</p4></div> </div>" +
-            "<div class=\"commentsection\">" + writtenReview + "</div></div></div>");
+          $("#commentsHolder").append("<div class=\"comment\"><div class=\"tagscontainer\"><div class=\"tagstitle\"><p5>" + date +
+          "</p5></div><div class=\"year\"><p5>" + year + "</p5></div><div class=\"tag\"><p5>" + culture + "</p5></div></div>" +
+          "<div class=\"commentcontainer\"><div class=\"dormrating\"><div class=\"commentrate\"><div class=\"row\"><div class=\"box4\">" +
+          "<p8>" + overall + "</p8></div><div class=\"description1\"><p8 class=\"info2\">OVERALL</p8></div></div>" +
+          "<div class=\"row\"><div class=\"box3\"><p7>" + roomPerc + "</p7></div><div class=\"description1\"><p7 class=\"info2\">ROOM</p7></div></div>" +
+          "<div class=\"row\"><div class=\"box3\"><p7>" + bathroomPerc + "</p7></div><div class=\"description1\"><p7 class=\"info2\">BATHROOM</p7></div></div>" +
+          "<div class=\"row\"><div class=\"box3\"><p7>" + kitchenPerc + "</p7></div><div class=\"description1\"><p7 class=\"info2\">KITCHEN</p7></div></div>" +
+          "</div></div>" + "<div class=\"commentsection\"><p9>" + writtenReview + "</p9></div></div></div>");
         }
       })
     }).then(function(){
       var finalPercentage = Math.round((grandScore/topTotalScorePossible)*100);
+      console.log("total party score is " + grandPartyDist);
       var avgPartyDist = Math.round(grandPartyDist/numReviews);
       var avgStudyDist = Math.round(grandStudyDist/numReviews);
       var avgGymDist = Math.round(grandGymDist/numReviews);
+      var avgKitchenScore = Math.round(grandKitchenScore/numReviews);
       $(".dormtitle").empty();
       $(".dormtitle").append("<h8>" + currentDormSelected + "</h8>");
       $(".left > h10").empty();
       $(".left > h10").append(finalPercentage + "%");
-      $("#distToParty").empty();
-      $("#distToParty").append(avgPartyDist + " min");
-      $("#distToStudy").empty();
-      $("#distToStudy").append(avgStudyDist + " min");
-      $("#distToGym").empty();
-      $("#distToGym").append(avgGymDist + " min");
+
+      var partyDistPerc = Math.round((avgPartyDist/15)*100);
+      console.log("party dist % is " + partyDistPerc);
+      $("#distToParty").width(partyDistPerc+"%")
+      $("#distToParty > p3").text(avgPartyDist + "min");
+
+      var studyDistPerc = Math.round((avgStudyDist/15)*100);
+      console.log("study dist % is " + studyDistPerc);
+      $("#distToStudy").width(studyDistPerc+"%");
+      $("#distToStudy > p3").text(avgStudyDist + "min");
+
+      var gymDistPerc = Math.round((avgGymDist/15)*100);
+      console.log("gym dist % is " + gymDistPerc);
+      $("#distToGym").width(gymDistPerc+"%");
+      $("#distToGym > p3").text(avgGymDist + "min");
+
+      $("#population").text(population + " RESIDENTS")
+      if(dormStyle == "Hall"){
+        $("#hallStyle").text("HALL STYLE");
+      }else{
+        $("#hallStyle").text("SUITE STYLE");
+      }
+
+      if(laundry == "yes"){
+        $("#laundry").text("LAUNDRY ROOMS");
+      }else{
+        $("#laundry").text("NO LAUNDRY ROOMS");
+      }
+
+
     })
   });
 }
--- public/javascript/writeReview.js
+++ public/javascript/writeReview.js
@@ -1,8 +1,11 @@
+var currentNumberOfRatings;
 var dormNames = ["Alderman", "Alexander", "Avery", "Aycock", "Carmichael",
-"Cobb","Connor", "Craige", "Craige-North", "Ehringhaus", "Everett", "Graham", "Grimes",
-"Hardin", "Hinton-James", "Horton", "Joyner", "Kenan", "Koury", "Lewis", "Mangum", "Manly",
-"McIver", "Morrison", "Old-East", "Old-West", "Parker", "Ruffin", "Spencer", "Stacy", "Teague",
-"Winston"];
+"Cobb","Connor", "Craige", "Craige North", "Ehringhaus", "Everett", "Graham", "Grimes",
+"Hardin", "Hinton James", "Horton", "Joyner", "Kenan", "Koury", "Lewis", "Mangum", "Manly",
+"McIver", "Morrison", "Old East", "Old West", "Parker", "Ruffin", "Spencer", "Stacy", "Teague",
+"Winston", ];
+
+var cultureArr = ["Rowdy", "Work-hard", "Play-hard", "Greek", "Chill"];
 
 for(var i=0; i<dormNames.length; i++){
   $("#dormName").append("<option id=\"" + dormNames[i].toLowerCase() + "\">" + dormNames[i] + "</option>");
@@ -49,7 +52,7 @@ $("#submit").click(function(){
     var yearInDorm = $("#yearSelector").find(":selected").text();
     var dorm = $("#dormName").find(":selected").text();
     var bathroomRating = $("#slider3").val();
-    var cleanlinessRating = $("#slider5").val();
+    var cleanlinessRating = $("#slider6").val();
     var kitchenRating = $("#slider4").val();
     var studyProximityRating = $("#slider1").val();
     var partyProximityRating = $("#slider2").val();
@@ -59,6 +62,7 @@ $("#submit").click(function(){
     var email = $("#email").val();
     var bathStyle = $("#dormStyle").find(":selected").text();
     var writtenReview = $("#textReview").val();
+    var roomRating = $("#slider7").val();
 
     if($("#hasLaundry").find(":selected").text() == "Yes"){
       hasLaundry = true;
@@ -70,7 +74,10 @@ $("#submit").click(function(){
       wouldLiveAgain = false;
     }
     var database = firebase.database();
-    var dormRatingNode = database.ref("/ratings/" + dorm + "/");
+    var dormRatingNode = database.ref("UNC-CH/ratings/" + dorm + "/");
+    var d = new Date();
+    var dateWritten = (d.getMonth()+1).toString() + "/" + d.getDate().toString() + "/" + d.getFullYear().toString();
+
     var thisPush = dormRatingNode.push();
     //set new data fields for this review
     thisPush.set({
@@ -87,18 +94,124 @@ $("#submit").click(function(){
       laundry: hasLaundry,
       wouldLiveHereAgain: wouldLiveAgain,
       bathroomStyle: bathStyle,
-      review: writtenReview
+      review: writtenReview,
+      room: roomRating,
+      date: dateWritten
     });
 
-    //update the number of ratings
     dormRatingNode.once("value").then(function(snap){
-      currentNumberOfRatings = parseInt(snap.val().numReviews);
-      currentNumberOfRatings = currentNumberOfRatings+1;
-      dormRatingNode.child("numReviews").set(currentNumberOfRatings);
-      window.location="../html/exitReview.html"
+      if(snap.hasChild("culture")){
+        var currentCulture = snap.child("culture").child(cultureReview).val();
+        currentCulture = currentCulture + 1;
+        dormRatingNode.child("culture").child(cultureReview).set(currentCulture);
+        var currentHighestVotes = snap.child("avgCulture").val();
+        if(currentCulture > snap.child("culture").child(currentHighestVotes)){
+          snap.child("avgCulture").set(cultureReview);
+        }
+      }else{
+        for(var i=0; i<cultureArr.length; i++){
+          if(cultureArr[i]!=cultureReview){
+            dormRatingNode.child("culture").child(cultureArr[i]).set(0);
+          }else{
+            dormRatingNode.child("culture").child(cultureReview).set(1);
+          }
+        }
+        dormRatingNode.child("avgCulture").set(cultureReview);
+      }
+    }).then(function(){
+        dormRatingNode.once("value").then(function(snap){//update the number of ratings
+        currentNumberOfRatings = parseInt(snap.val().numReviews);
+        alert("current number of ratings is " + currentNumberOfRatings);
+        currentNumberOfRatings = currentNumberOfRatings+1;
+        dormRatingNode.child("numReviews").set(currentNumberOfRatings);
+        alert("current number of updated ratings is " + currentNumberOfRatings);
+      }).then(function(){ //Update the averages for this dorm.
+      var avgGymDist;
+      var avgKitchen;
+      var avgBathroom;
+      var avgCleanliness;
+      var avgStudyDist;
+      var avgPartyDist;
+      var avgCulture;
+      var avgRoom;
+      dormRatingNode.once("value").then(function(snapshot){
+          if(snapshot.hasChild("avgGymDist")){
+            avgGymDist = snapshot.child("avgGymDist").val();
+            avgGymDist = (avgGymDist)*(currentNumberOfRatings-1) + parseInt(gymProximityRating);
+            avgGymDist = Math.round(avgGymDist/currentNumberOfRatings);
+            alert("average gym distance is " + avgGymDist + "; current number of ratings is " + currentNumberOfRatings);
+            dormRatingNode.child("avgGymDist").set(parseInt(avgGymDist));
+          }else{
+            dormRatingNode.child("avgGymDist").set(parseInt(gymProximityRating));
+          }
+          if(snapshot.hasChild("avgKitchen")){
+            avgKitchen = snapshot.child("avgKitchen").val();
+            avgKitchen = (avgKitchen)*(currentNumberOfRatings-1) + parseInt(kitchenRating);
+            avgKitchen = Math.round(avgKitchen/currentNumberOfRatings);
+            dormRatingNode.child("avgKitchen").set(parseInt(avgKitchen));
+          }else{
+            dormRatingNode.child("avgKitchen").set(parseInt(kitchenRating));
+          }
+          if(snapshot.hasChild("avgBathroom")){
+            avgBathroom = snapshot.child("avgBathroom").val();
+            avgBathroom = (avgBathroom)*(currentNumberOfRatings -1) + parseInt(bathroomRating);
+            avgBathroom = Math.round(avgBathroom/currentNumberOfRatings);
+            dormRatingNode.child("avgBathroom").set(parseInt(avgBathroom));
+          }else{
+            dormRatingNode.child("avgBathroom").set(parseInt(bathroomRating));
+          }
+          if(snapshot.hasChild("avgCleanliness")){
+            avgCleanliness = snapshot.child("avgCleanliness").val();
+            avgCleanliness = (avgCleanliness)*(currentNumberOfRatings - 1) + parseInt(cleanlinessRating);
+            avgCleanliness = Math.round(avgCleanliness/currentNumberOfRatings);
+            dormRatingNode.child("avgCleanliness").set(parseInt(avgCleanliness));
+          }else{
+            dormRatingNode.child("avgCleanliness").set(parseInt(cleanlinessRating));
+          }
+          if(snapshot.hasChild("avgStudyDist")){
+            avgStudyDist = snapshot.child("avgStudyDist").val();
+            alert("average study dist before any computation is " + avgStudyDist);
+            avgStudyDist = (avgStudyDist)*(currentNumberOfRatings - 1) + parseInt(studyProximityRating);
+            alert("average study dist after multiplication and addition of new study score is " + avgStudyDist);
+            avgStudyDist = Math.round(avgStudyDist/currentNumberOfRatings);
+            alert("average Study distance after full computation is " + avgStudyDist);
+            dormRatingNode.child("avgStudyDist").set(parseInt(avgStudyDist));
+          }else{
+            dormRatingNode.child("avgStudyDist").set(parseInt(studyProximityRating));
+          }
+
+          if(snapshot.hasChild("avgPartyDist")){
+            avgPartyDist = snapshot.child("avgPartyDist").val();
+            avgPartyDist = (avgPartyDist)*(currentNumberOfRatings - 1) + parseInt(partyProximityRating);
+            avgPartyDist = Math.round(avgPartyDist/currentNumberOfRatings);
+            dormRatingNode.child("avgPartyDist").set(parseInt(avgPartyDist));
+          }else{
+            dormRatingNode.child("avgPartyDist").set(parseInt(partyProximityRating));
+          }
+
+          var numOfCulturePoints = snapshot.child("culture").child(cultureReview).val();
+          numOfCulturePoints = numOfCulturePoints + 1;
+          dormRatingNode.child("culture").child(cultureReview).set(numOfCulturePoints);
+          var currentPopularCulture = snapshot.child("avgCulture").val();
+          var currentVotesMostPopular = snapshot.child("culture").child(currentPopularCulture).val();
+          if(numOfCulturePoints > currentVotesMostPopular){
+            dormRatingNode.child("avgCulture").set(cultureReview);
+          }
+
+          if(snapshot.hasChild("avgRoom")){
+            avgRoom = snapshot.child("avgRoom").val();
+            avgRoom = (avgRoom)*(currentNumberOfRatings - 1) + parseInt(roomRating);
+            avgRoom = Math.round(avgRoom/currentNumberOfRatings);
+            dormRatingNode.child("avgRoom").set(parseInt(avgRoom));
+          }else{
+            dormRatingNode.child("avgRoom").set(parseInt(roomRating));
+          }
+        }).then(function(){
+          window.location = "../html/exitReview.html";
+        });
+      })
     })
-  //}
-})
+  });
 
 function checkForInputs(){
   //return true or false depending on whether or not values from inputs have been chosen
